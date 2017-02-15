4daf3fd182ed4c8c596abbc1bd2caba98dabd527
wuhongxu <1102612827@qq.com>
2017-02-16 00:27:41 +0800

    init

---
+++ src/main/java/com/zido/BingoApplication.java
@@ -0,0 +1,19 @@
+package com.zido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * bingo.
+ * Date: 2017/2/16 0016
+ * Time: 0:24
+ *
+ * @author <a href="http://userwu.github.io">wuhongxu</a>.
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class BingoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BingoApplication.class,args);
+    }
+}
